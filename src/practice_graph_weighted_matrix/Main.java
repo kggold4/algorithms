1298bea32d6de5b8cde91b95e099a5238af3733c
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-04-21 14:37:47 +0300

    update main

--- src/practice_graph_weighted_matrix/Main.java
+++ src/practice_graph_weighted_matrix/Main.java
@@ -2,9 +2,10 @@ package practice_graph_weighted_matrix;
 
 public class Main {
     public static void main(String[] args) {
-        weighted_graph_matrix m = new GraphWeightedMatrix(5);
+        weighted_graph_matrix m = new GraphWeightedMatrix(10);
         m.connect(1, 2, 15);
         m.connect(2, 4, 12);
+        m.connect(8, 4, 7);
         System.out.println(m);
         m.floyd_warshall();
         System.out.println(m);
