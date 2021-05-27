ba62ecea16c164812a10a2968d32afc1428c7f34
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-05-27 19:40:03 +0300

    add merge sort for linked lists on data-structures algorithims

--- src/data_structures/linkedlist/Main.java
+++ src/data_structures/linkedlist/Main.java
@@ -6,18 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         // testing reverse linked list function
-        Node head = new Node();
-        Node next = new Node();
-        head.next = next;
+        Node head1 = new Node();
+        Node next1 = new Node();
+        head1.next = next1;
         int i = 0;
         while(i < NUMBER_OF_NODES) {
-            next.next = new Node();
-            next = next.next;
+            next1.next = new Node();
+            next1 = next1.next;
             i++;
         }
-        print_linked_list(head);
-        Node new_head = Reverse.reverse_linked_list(head);
-        print_linked_list(new_head);
+        print_linked_list(head1);
+        Node new_head1 = Reverse.reverse_linked_list(head1);
+        print_linked_list(new_head1);
+
+        // testing sort linked list function
+        Node head2 = new Node(3);
+        head2.next = new Node(2);
+        head2.next.next = new Node(5);
+        head2.next.next.next = new Node(4);
+        head2.next.next.next.next = new Node(1);
+        head2.next.next.next.next.next = new Node(6);
+        print_linked_list(head2);
+        Node new_head2 = Sort.merge_sort_linked_list(head2);
+        print_linked_list(new_head2);
+
     }
 
     private static void print_linked_list(Node node) {
---
+++ src/data_structures/linkedlist/Sort.java
@@ -0,0 +1,72 @@
+package data_structures.linkedlist;
+
+public class Sort {
+
+    /**
+     * merge sort on linked list
+     * @param head
+     * @return
+     */
+    public static Node merge_sort_linked_list(Node head) {
+
+        // base case
+        if (head == null || head.next == null) return head;
+
+        // get the middle of the list
+        Node middle = get_middle(head);
+
+        // set the next of middle node to null
+        middle.next = null;
+
+        // Apply mergeSort on left list
+        Node left = merge_sort_linked_list(head);
+
+        // Apply mergeSort on right list
+        Node right = merge_sort_linked_list(middle.next);
+
+        // Merge the left and right lists
+        Node sortedlist = sorted_merge(left, right);
+        return sortedlist;
+    }
+
+    /**
+     * return the middle node of a linked list
+     * @param head
+     * @return
+     */
+    private static Node get_middle(Node head) {
+
+        // base case
+        if(head == null) return head;
+
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * sorted merge on two given nodes
+     * @param a
+     * @param b
+     * @return
+     */
+    private static Node sorted_merge(Node a, Node b) {
+        Node result = null;
+
+        // base cases
+        if(a == null) return b;
+        if(b == null) return a;
+
+        if (a.data <= b.data) {
+            result = a;
+            result.next = sorted_merge(a.next, b);
+        } else {
+            result = b;
+            result.next = sorted_merge(a, b.next);
+        }
+        return result;
+    }
+}
