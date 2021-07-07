2e0f1e2bd15b8aed7f6e9cf462198ae96223def5
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-07-07 18:03:12 +0300

    add counting sort

---
+++ src/data_structures/sorting/CountingSort.java
@@ -0,0 +1,24 @@
+package data_structures.sorting;
+
+public class CountingSort {
+
+    /**
+     * Max Sort
+     * @param A
+     */
+    public static void countingSort(int[] A) {
+
+        int max = 0, index = 0;
+        for(int i : A) if(max < i) max = i;
+        int[] freq = new int[max+1];
+        for(int i = 0; i < A.length; i++) freq[A[i]]++;
+        for(int i = 0; i <= max; i++) {
+            int m = 0;
+            while(m < freq[i]) {
+                A[index] = i;
+                index++;
+                m++;
+            }
+        }
+    }
+}
